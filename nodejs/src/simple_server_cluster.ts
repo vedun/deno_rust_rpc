6a316e1da6276d670bb6d2517975b9896875eb73
vedun <vedun.mail@gmail.com>
2023-06-13 18:33:41 +0300

    test with cluster

---
+++ nodejs/src/simple_server_cluster.ts
@@ -0,0 +1,25 @@
+import { availableParallelism } from 'node:os';
+import { createServer } from 'node:http';
+import cluster from 'node:cluster';
+
+const data = JSON.stringify({ name: 'user', age: 12 });
+
+const numCPUs = availableParallelism();
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    const pid = worker.process.pid ?? 'undefinde';
+    console.log(`worker ${pid} exited`);
+  });
+} else {
+  const srv = createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(data);
+  });
+  srv.listen({ port: 8000 });
+  console.log(`Worker ${process.pid} started`);
+}
