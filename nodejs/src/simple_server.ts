bf570591ffa97e5fc6f789df11ab2a5e776cc0c6
vedun <vedun.mail@gmail.com>
2023-06-13 18:47:32 +0300

    updates

--- nodejs/src/simple_server.ts
+++ nodejs/src/simple_server.ts
@@ -1,11 +1,22 @@
 import * as utils from 'node:util';
+import * as path from 'node:path';
 import { createServer } from 'node:http';
 import { encode, decode } from '@msgpack/msgpack';
 import { Rpc } from './rpc.js';
 
 // const data = JSON.stringify({ name: 'user', age: 12 });
 
-const rpc = new Rpc('/home/mks/projects/isone2/src/node_rpc_rust/rust/target/release/simple_rpc');
+const binaryPath = path.join(
+  path.dirname(new URL(import.meta.url).pathname),
+  '..',
+  '..',
+  'rust',
+  'target',
+  'release',
+  'simple_rpc',
+);
+
+const rpc = new Rpc(binaryPath);
 
 const srv = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
@@ -20,5 +31,4 @@ const srv = createServer((req, res) => {
       res.end(`{"error": "${utils.inspect(e)}"}`);
     });
 });
-
-srv.listen({ port: 8000, exclusive: false });
+srv.listen({ port: 8000 });
